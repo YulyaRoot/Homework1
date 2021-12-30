9766ace5daeadf4b4c824097dca820d57ca4ac1b
Yulya Kondrateva <kond.yulya@gmail.com>
2021-12-31 00:11:00 +0300

    Added: Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string strA = Console.ReadLine();
+int a = int.Parse(strA);
+Console.Write("Введите второе число: ");
+string strB = Console.ReadLine();
+int b = int.Parse(strB);
+
+if (a == b * b)
+{
+    Console.WriteLine($"Число {a} является квадратом {b}");
+}
+else 
+{
+    Console.WriteLine($"Число {a} не является квадратом {b}");
+}
