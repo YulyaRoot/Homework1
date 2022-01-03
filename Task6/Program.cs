8210f0e13b74d5cdbc91dd6636f8dd289b198db0
Yulya Kondrateva <kond.yulya@gmail.com>
2022-01-03 21:31:00 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = new Random().Next(10, 100);
+Console.WriteLine($"Дано число {n}");
+string s = n.ToString();
+
+int a = int.Parse(s[0].ToString());
+int b = int.Parse(s[1].ToString());
+
+if (a > b)
+{
+    Console.WriteLine($"Наибольшая цифра {a}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра {b}");
+}
