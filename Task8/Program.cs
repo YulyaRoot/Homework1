1f28239f5f1d78682835a1088e14dc92c5a73f1f
Yulya Kondrateva <kond.yulya@gmail.com>
2022-01-05 16:52:10 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+string strA = Console.ReadLine();
+int a = int.Parse(strA);
+Console.Write("Введите иторое число: ");
+string strB = Console.ReadLine();
+int b = int.Parse(strB);
+int r = a % b;
+
+if (r == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine("Число не кратно");
+    Console.WriteLine($"Остаток {r} ");
+}
+
